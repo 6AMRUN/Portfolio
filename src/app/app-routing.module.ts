3165114920c19ccd29d6ce56507cd461bdccc11c
lex <krisafarinas@gmail.com>
2023-05-26 19:32:03 -0700

    Updating Portfolio to use framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+const routes: Routes = [
+  { path: "", component: ProjectsComponent },
+  { path: "skills", component: SkillsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SkillsComponent } from './components/skills/skills.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    HeaderComponent,
+    ModalComponent,
+    ProjectsComponent
   ],
   imports: [
+    NgModule,
     BrowserModule,
     AppRoutingModule
   ],
